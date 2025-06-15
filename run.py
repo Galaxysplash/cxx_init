a30a058a729a1f45a6b191ffa3883673e4c051ed
marcel <marcel.hajek@gmx.de>
2025-06-15 21:08:22 +0200

    ...

--- run.py
+++ run.py
@@ -10,7 +10,9 @@ else:
 
 subprocess.run("mkdir build", shell=True)
 
-subprocess.run("conan install . --build=missing -of build && cmake . -B build && make -C build", check=True, shell=True)
+result = subprocess.run("conan install . --build=missing -of build && cmake . -B build && make -C build", check=True, shell=True)
+
+print(result.stderr.decode())
 
 if platform.system() == "Windows":
     subprocess.run([f".\\build\\{app_name}.exe"], check = True)
