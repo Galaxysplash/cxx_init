c73da39e62aae97a1c3e41d40037f1ba23911087
marcel <marcel.hajek@gmx.de>
2025-06-15 16:17:29 +0200

    ...

--- src/program.cpp
+++ src/program.cpp
@@ -21,51 +21,55 @@ auto Program::execute(const std::string &project_name) -> void
     std::stringstream
         main_cpp_content;
 
-    main_cpp_content << "#include <iostream>\n\n"
-                     << "int main(int argc, const char* argv[])\n"
-                     << "{\n"
-                     << "\tstd::cout << \"Hello World!\\n\";\n\n"
-                     << "\treturn 0;\n"
-                     << "}";
+    main_cpp_content
+        << "#include <iostream>\n\n"
+        << "int main(int argc, const char* argv[])\n"
+        << "{\n"
+        << "\tstd::cout << \"Hello World!\\n\";\n\n"
+        << "\treturn 0;\n"
+        << "}";
 
-    cmakelists_content << "cmake_minimum_required(VERSION 3.20)\n"
-                       << "project(" << project_name << ")\n"
-                       << "\n"
-                       << "set(CMAKE_CXX_STANDARD 20)\n"
-                       << "set(CMAKE_CXX_STANDARD_REQUIRED ON)\n"
-                       << "\n"
-                       << "file(GLOB_RECURSE SOURCES src/*.cpp)\n"
-                       << "add_executable(${PROJECT_NAME} ${SOURCES})\n";
+    cmakelists_content
+        << "cmake_minimum_required(VERSION 3.20)\n"
+        << "project(" << project_name << ")\n"
+        << "\n"
+        << "set(CMAKE_CXX_STANDARD 20)\n"
+        << "set(CMAKE_CXX_STANDARD_REQUIRED ON)\n"
+        << "\n"
+        << "file(GLOB_RECURSE SOURCES src/*.cpp)\n"
+        << "add_executable(${PROJECT_NAME} ${SOURCES})\n";
 
-    python_run_file_content << "import platform\n"
-                            << "import subprocess\n\n"
-                            << "app_name: str = \"" << project_name << "\"\n\n"
-                            << "if platform.system() == \"Windows\":\n"
-                            << "\tsubprocess.run(\"rmdir /S /Q build\", shell = True)\n"
-                            << "else:\n"
-                            << "\tsubprocess.run(\"rm -rf build\", shell = True)\n\n"
-                            << "subprocess.run(\"mkdir build\", shell=True)\n\n"
-                            << "subprocess.run("
-                            << "f\"" << "conan install . --build=missing -of build "
-                            << "&& cmake . -B build && make - C build"
-                            << "\""
-                            << ", check=True, shell=True)\n\n"
-                            << "if platform.system() == \"Windows\":\n"
-                            << "\tsubprocess.run([f\".\\\\build\\\\{app_name}.exe\"], check = True)\n"
-                            << "else:\n"
-                            << "\tsubprocess.run([f\"./build/{app_name}\"], check = True)\n";
+    python_run_file_content
+        << "import platform\n"
+        << "import subprocess\n\n"
+        << "app_name: str = \"" << project_name << "\"\n\n"
+        << "if platform.system() == \"Windows\":\n"
+        << "\tsubprocess.run(\"rmdir /S /Q build\", shell = True)\n"
+        << "else:\n"
+        << "\tsubprocess.run(\"rm -rf build\", shell = True)\n\n"
+        << "subprocess.run(\"mkdir build\", shell=True)\n\n"
+        << "subprocess.run("
+        << "f\"" << "conan install . --build=missing -of build "
+        << "&& cmake . -B build && make -C build"
+        << "\""
+        << ", check=True, shell=True)\n\n"
+        << "if platform.system() == \"Windows\":\n"
+        << "\tsubprocess.run([f\".\\\\build\\\\{app_name}.exe\"], check = True)\n"
+        << "else:\n"
+        << "\tsubprocess.run([f\"./build/{app_name}\"], check = True)\n";
 
-    readme_file_content << "# " << project_name
-                        << "\n\n"
-                        << "## prerequisites\n"
-                        << "->python3<br>\n"
-                        << "->git<br>\n"
-                        << "->make<br>\n"
-                        << "->cmake 3.20 or higher\n\n"
-                        << "## build & run\n"
-                        << "-> On Windows: 'python run.py'<br>\n"
-                        << "-> On unix-like systems like (macOS, Linux, freeBSD, ...): "
-                        << "'python3 run.py'<br>\n";
+    readme_file_content
+        << "# " << project_name
+        << "\n\n"
+        << "## prerequisites\n"
+        << "->python3<br>\n"
+        << "->git<br>\n"
+        << "->make<br>\n"
+        << "->cmake 3.20 or higher\n\n"
+        << "## build & run\n"
+        << "-> On Windows: 'python run.py'<br>\n"
+        << "-> On unix-like systems like (macOS, Linux, freeBSD, ...): "
+        << "'python3 run.py'<br>\n";
 
     dot_gitignore_content
         << "build\n"
@@ -87,7 +91,8 @@ auto Program::execute(const std::string &project_name) -> void
         {"README.md", readme_file_content.str()},
         {"conanfile.txt", conan_file_txt_content.str()}};
 
-    File src_file = {"main.cpp", main_cpp_content.str()};
+    File src_file =
+        {"main.cpp", main_cpp_content.str()};
 
     create_folders(project_directory, folder_names);
 
