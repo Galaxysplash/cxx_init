feb3125157edbc14e8e5f5a065e9b9550ee5c9ec
Galaxy Master <Marcel.Hajek@gmx.de>
2025-06-20 18:28:14 +0200

    ...

---
+++ tmp_test_project/run.py
@@ -0,0 +1,70 @@
+import os
+import platform
+from subprocess import run, PIPE
+
+# how your executable should be named
+project_name: str = "tmp_test_project"
+
+# arguments you wonna use for testing
+testing_arguments = ""
+
+
+# build code (don't touch it, unless you know what your doing)
+windows: bool = platform.system() == "Windows"
+
+if windows:
+	run("rmdir /S /Q build", shell = True)
+else:
+	run("rm -rf build", shell = True)
+
+if not os.path.exists("build"):
+	run("mkdir build", shell=True)
+
+def execute_cmd(cmd: str, description: str):
+	conan_result = run(
+		cmd,
+		shell=True,
+		stdout=PIPE,
+		stderr=PIPE
+	)
+
+	if conan_result.returncode == 0:
+		print(description + " was successful!!!\n\n\n")
+	else:
+		print("---DETAIL ZONE---")
+		if conan_result.stdout:
+			print(conan_result.stdout)
+		if conan_result.stderr:
+			print(conan_result.stderr)
+		print("---END DETAIL ZONE---")
+		
+		print("\n\n\n" + description + " has failed! (details are above)")
+		exit(1)
+
+
+
+cmds = [
+			(
+				"conan install . --build=missing -of build",
+				"Conan installing packages required by your conanfile.txt"
+			),
+			(
+				"cmake . -B build",
+				"CMake pre generating the stuff necessary for building your project"
+			),
+			(
+				"make -C build",
+				"Make trying to build your project form the cmake generated stuff"
+			)
+		]
+
+for cmd in cmds:
+	execute_cmd(cmd[0], cmd[1])
+
+if windows:
+	if os.path.exists(f".\\build\\{project_name}_exec.exe"):
+		run([f".\\build\\{project_name}_exec.exe {testing_arguments}"], shell=True, check = True)
+else:
+	if os.path.exists(f"./build/{project_name}_exec"):
+		run([f"./build/{project_name}_exec {testing_arguments}"], shell=True, check = True)
+# end building code
