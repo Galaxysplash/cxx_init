d1e99462c9f7d90c87cbea42abebb93d1f5c1e68
Galaxy Master <Marcel.Hajek@gmx.de>
2025-06-20 20:19:51 +0200

    ...

--- include/conanfile_txt.h
+++ include/conanfile_txt.h
@@ -1,3 +1,3 @@
 #pragma once
 
-constexpr const char *conanfile_txt = "# uncomment for external dependencies\n# [requires]\n# dependency\n\n[generators]\nCMakeDeps\nCMakeToolchain";
+constexpr const char *conanfile_txt = "# uncomment for external dependencies\n# [requires]\n# dependency 1\n# dependency 2\n# ...\n\n[generators]\nCMakeDeps\nCMakeToolchain";
